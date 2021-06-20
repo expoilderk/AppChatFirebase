32ef4c00cdd08a2a4961bdbaf30feda3c2fda3c7
expoilderk <falecommarlonreis@gmail.com>
2021-06-20 12:17:36 -0300

    Implementando Layout, Autenticação, Upload de fotos no storage, cloud firestore

---
+++ app/src/main/java/br/com/android/firebasechat/LoginActivity.java
@@ -0,0 +1,75 @@
+package br.com.android.firebasechat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText mEditEmail;
+    private EditText mEditPassword;
+    private Button mBtnEnter;
+    private TextView mTxtAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEditEmail = findViewById(R.id.edit_email);
+        mEditPassword = findViewById(R.id.edit_password);
+        mBtnEnter = findViewById(R.id.btn_enter);
+        mTxtAccount = findViewById(R.id.txt_account);
+
+        mBtnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = mEditEmail.getText().toString();
+                String password = mEditPassword.getText().toString();
+
+                Log.i("Teste", email);
+                Log.i("Teste", password);
+
+                if (email == null || email.isEmpty() || password == null || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Nome, Email e Senha devem ser preenchidos", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.i("Teste", task.getResult().getUser().getUid());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.i("Teste", e.getMessage());
+                            }
+                        });
+            }
+        });
+
+        mTxtAccount.setOnClickListener((v) -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
+
+    }
+}
